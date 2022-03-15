ebd60b73c9f09f6e133b0a516567ddce99ce2f8b
Silvia Frigatto <59895509+silviafrigatto@users.noreply.github.com>
2022-03-15 16:14:57 -0300

    Finishing first version

--- battleship.js
+++ battleship.js
@@ -1,6 +1,7 @@
-var location1 = 3;
-var location2 = 4;
-var location3 = 5;
+var randomLoc = Math.floor(Math.random() * 5);
+var location1 = randomLoc;
+var location2 = location1 + 1;
+var location3 = location2 + 1;
 var guess;
 var hits = 0;
 var guesses = 0;
@@ -12,17 +13,27 @@ while (isSunk == false) {
 
     if (guess < 0 || guess > 6) {
         alert("Please enter a valid cell number!");
+    }
+
+    else {
+        guesses += 1;
+
+        if (guess == location1 || guess == location2 || guess == location3) {
+        
+            alert("HIT!");
+            hits += 1;
+
+            if (hits == 3) {
+                isSunk = true;
+                alert("You sank the battleship!");
+            }
         }
 
         else {
-        guesses =+ 1;
-        if (guess == location1 || guess == location2 || guess == location3) {
-            hits += 1;
+            alert("MISS!");
         }
-    }
-
-    if (hits == 3) {
-        isSunk = true;
-        alert("You sank the battleship!");
-    }
+    }   
 }
+
+var stats = "You took " + guesses + " guesses to sink the battleship." + "\nYour shooting accuracy was: " + (3 / guesses);
+alert(stats);
