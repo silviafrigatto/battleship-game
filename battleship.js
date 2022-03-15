942e42215935777d6eaa6de0552324142c4c64d3
Silvia Frigatto <59895509+silviafrigatto@users.noreply.github.com>
2022-03-15 14:25:57 -0300

    Battleship sank

--- battleship.js
+++ battleship.js
@@ -12,11 +12,17 @@ while (isSunk == false) {
 
     if (guess < 0 || guess > 6) {
         alert("Please enter a valid cell number!");
-        } 
+        }
+
         else {
         guesses =+ 1;
         if (guess == location1 || guess == location2 || guess == location3) {
             hits += 1;
         }
+    }
+
+    if (hits == 3) {
+        isSunk = true;
+        alert("You sank the battleship!");
     }
 }
